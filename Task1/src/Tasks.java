30b65af4703b4b82d49dff9f1c5a8c759cf6c0fc
Kweall <90971322+Kweall@users.noreply.github.com>
2022-10-19 17:23:27 +0300

    Update Tasks.java

--- Task1/src/Tasks.java
+++ Task1/src/Tasks.java
@@ -1,48 +1,48 @@
 public class Tasks{
     public static void main(String args[]) throws Exception{
         print(1,"remainder");
-        System.out.println(remainder(1,3));
-        System.out.println(remainder(3,4));
-        System.out.println(remainder(-9,45));
-        System.out.println(remainder(5,5));
+        System.out.println(remainder(1,3)); // 1
+        System.out.println(remainder(3,4)); // 3
+        System.out.println(remainder(-9,45)); // -9
+        System.out.println(remainder(5,5)); // 0
         print(2,"triArea");
-        System.out.println(triArea(3,2));
-        System.out.println(triArea(7,4));
-        System.out.println(triArea(10,10));
+        System.out.println(triArea(3,2)); // 3
+        System.out.println(triArea(7,4)); // 14
+        System.out.println(triArea(10,10)); // 50
         print(3, "animals");
-        System.out.println(animals(2,3,5));
-        System.out.println(animals(1,2,3));
-        System.out.println(animals(5,2,8));
+        System.out.println(animals(2,3,5)); // 36
+        System.out.println(animals(1,2,3)); // 22
+        System.out.println(animals(5,2,8)); // 50
         print(4, "profitableGamble");
-        System.out.println(profitableGamble(0.2,50,9));
-        System.out.println(profitableGamble(0.9,1,2));
-        System.out.println(profitableGamble(0.9,3,2));
+        System.out.println(profitableGamble(0.2,50,9)); // true
+        System.out.println(profitableGamble(0.9,1,2)); // false
+        System.out.println(profitableGamble(0.9,3,2)); // true
         print(5, "operation");
-        System.out.println(operation(24,15,9));
-        System.out.println(operation(24,26,2));
-        System.out.println(operation(15,11,11));
+        System.out.println(operation(24,15,9)); // "added"
+        System.out.println(operation(24,26,2)); // "subtracted"
+        System.out.println(operation(15,11,11)); // "none"
         print(6, "ctoa");
-        System.out.println(ctoa('A'));
-        System.out.println(ctoa('m'));
-        System.out.println(ctoa('['));
-        System.out.println(ctoa('\\'));
+        System.out.println(ctoa('A')); // 65
+        System.out.println(ctoa('m')); // 109
+        System.out.println(ctoa('[')); // 91
+        System.out.println(ctoa('\\')); // 92
         print(7,"addUpTo");
-        System.out.println(addUpTo(3));
-        System.out.println(addUpTo(10));
-        System.out.println(addUpTo(7));
+        System.out.println(addUpTo(3)); // 6
+        System.out.println(addUpTo(10)); // 55
+        System.out.println(addUpTo(7)); // 28
         print(8, "nextEdge");
-        System.out.println(nextEdge(8,10));
-        System.out.println(nextEdge(5,7));
-        System.out.println(nextEdge(9,2));
+        System.out.println(nextEdge(8,10)); // 17
+        System.out.println(nextEdge(5,7)); // 11
+        System.out.println(nextEdge(9,2)); // 10
         print(9, "sumOfCubes");
-        System.out.println(sumOfCubes(new int[]{1,5,9}));
-        System.out.println(sumOfCubes(new int[]{3,4,5}));
-        System.out.println(sumOfCubes(new int[]{2}));
-        System.out.println(sumOfCubes(new int[]{}));
+        System.out.println(sumOfCubes(new int[]{1,5,9})); // 855
+        System.out.println(sumOfCubes(new int[]{3,4,5})); // 216
+        System.out.println(sumOfCubes(new int[]{2})); // 8
+        System.out.println(sumOfCubes(new int[]{})); // 0
         print(10, "abcmath");
-        System.out.println(abcmath(42,5,10));
-        System.out.println(abcmath(5,2,1));
-        System.out.println(abcmath(1,2,3));
+        System.out.println(abcmath(42,5,10)); // false
+        System.out.println(abcmath(5,2,1)); // true
+        System.out.println(abcmath(1,2,3)); // false
     }
 
     private static boolean abcmath(int a, int b, int c) {
